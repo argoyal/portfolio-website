876c6966c4a57eca6f65897f5cbf4959c3129bb3
Arpit Goyal <arpitgoyal.iitkgp@outlook.com>
2025-08-28 22:15:55 +0530

    to handle production routes in next js client side

--- next.config.mjs
+++ next.config.mjs
@@ -10,6 +10,8 @@ const nextConfig = {
     unoptimized: true,
   },
   output: 'standalone',
+  trailingSlash: true,
+  assetPrefix: process.env.NODE_ENV === 'production' ? process.env.NEXT_PUBLIC_BASE_URL : '',
 }
 
 export default nextConfig
